2ee7687c2747df31633fd73d94786df66a32a751
Johan Persson <x-johan.persson@modelon.com>
2020-04-22 16:46:39 +0200

    [fix] better workflow

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+const path = require("path");
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    webpack: {
+      myConfig: {
+        entry: {
+          "pressure-cycle": "./src/pressure-cycle/index.js"
+        },
+        output: {
+          filename: "[name]/main.js",
+          path: path.resolve(__dirname, "dist")
+        }
+      }
+    },
+
+    copy: {
+      all: {
+        cwd: "src",
+        src: ["**/*.html", "**/*.json"],
+        dest: "dist",
+        expand: true
+      }
+    },
+
+    clean: ["dist"],
+
+    watch: {
+      js: {
+        files: ["src/**/*.js"],
+        tasks: ["webpack"]
+      },
+      copy: {
+        files: ["src/**/*.html", "src/**/*.json"],
+        tasks: ["copy"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-webpack");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("build", ["webpack", "copy"]);
+
+  grunt.registerTask("all", ["clean", "build"]);
+  grunt.registerTask("default", ["all"]);
+};
