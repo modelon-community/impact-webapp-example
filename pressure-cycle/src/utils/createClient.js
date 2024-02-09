f02250bd09bcc888eccfba12a1812da6282fffdb
Marcus Åberg <marcus.aberg@modelon.com>
2024-02-09 13:27:35 +0000

    fix: read impactToken from cookie

--- pressure-cycle/src/utils/createClient.js
+++ pressure-cycle/src/utils/createClient.js
@@ -1,17 +1,23 @@
 import { Client } from '@modelon/impact-client-js';
 
 const createClient = async () => {
-  const { jupyterHubToken, jupyterHubUserPath } = await getJupyterHubTokensAndUserPath();
+  const { impactToken, jupyterHubToken, jupyterHubUserPath } = await getJupyterHubTokensAndUserPath();
   // TODO: Client refreshes the impactToken if null -> it might still timeout? Client.fromCookie or similar should exist
   // TODO: Need to set token and path (at least in dev mode) -> should also be possible to calculate in client
-  const client = Client.fromImpactToken({ impactToken: null, jupyterHubToken, jupyterHubUserPath }); 
+  const client = Client.fromImpactToken({ impactToken, jupyterHubToken, jupyterHubUserPath }); 
   return client;
 };
 
 const getJupyterHubTokensAndUserPath = async () => {
+  const impactToken = getCookieValue("access_token");
   const jupyterHubUserPath = getJupyterHubUserPath();
   const jupyterHubToken = await getJupyterHubToken();
-  return { jupyterHubToken, jupyterHubUserPath };
+  return { impactToken, jupyterHubToken, jupyterHubUserPath };
+}
+
+const getCookieValue = (key) =>  {
+  const parts = `; ${document.cookie}`.split(`; ${key}=`);
+  return parts.length === 2 ? parts.pop().split(";").shift() : undefined;
 }
 
 const getJupyterHubUserPath = () => window.location.pathname.match(/\/user\/[^\/]+\//)[0] + "impact/"; 
