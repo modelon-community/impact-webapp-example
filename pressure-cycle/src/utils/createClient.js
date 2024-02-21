372645ec5f6e5ce3bcb965d50af42d3fdabacd3b
Marcus Åberg <marcus.aberg@modelon.com>
2024-02-21 09:28:35 +0000

    chore: ommit user-path and server address in deployment mode

--- pressure-cycle/src/utils/createClient.js
+++ pressure-cycle/src/utils/createClient.js
@@ -7,11 +7,10 @@ const createClient = async () => {
       impactApiKey: process.env.MODELON_IMPACT_API_KEY, 
       jupyterHubToken: process.env.JUPYTERHUB_API_TOKEN}); 
   } else {
-    const impactToken = getCookieValue('access_token'); // TODO: Cookie times out after a while and we need to log back in -> should also be solved by API Gateway
-    const jupyterHubToken = process.env.JUPYTERHUB_API_TOKEN; // TODO: This is a hack -> but is solved by API Gateway when we get that
-    const jupyterHubUserPath = window.location.pathname.split("impact/")[0];
-    const serverAddress = window.location.origin;
-    return Client.fromImpactToken({impactToken, jupyterHubToken, jupyterHubUserPath, serverAddress});
+    return Client.fromImpactToken({
+      impactToken: getCookieValue('access_token'), // TODO: Cookie times out after a while and we need to log back in -> should also be solved by API Gateway with impact-session
+      jupyterHubToken: process.env.JUPYTERHUB_API_TOKEN, // TODO: This is a hack, we should never compile the token into the app -> but should be solved by API Gateway with impact-session
+    });
   }
 };
 
