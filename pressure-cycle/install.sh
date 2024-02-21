1ef7f0a82a453e17510478a9dc2783ced8d267ce
Marcus Åberg <marcus.aberg@modelon.com>
2024-02-21 09:37:15 +0000

    chore: remove commented code from install.sh

--- pressure-cycle/install.sh
+++ pressure-cycle/install.sh
@@ -9,10 +9,6 @@ impact_customizations_dir="$IMPACT_BASEDIR/customizations"
 source_dir="$script_dir/dist"
 target_dir="$impact_customizations_dir/$script_dir_name"
 
-# Create symbolic link
-# TODO: Impact does not pick up "linked" apps - copying instead for now
-# ln -s "$source_dir" "$target_dir"
-
 # Copy /dist into customizations/$current_dir_name
 rm -rf "$target_dir"
 cp -rf "$source_dir" "$target_dir"
